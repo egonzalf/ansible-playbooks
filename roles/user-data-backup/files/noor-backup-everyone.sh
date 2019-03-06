ac5c630876b79beea20db28488e6741aadc3701e
Eduardo Gonzalez Fisher <eduardo.gonzalezfisher@kaust.edu.sa>
2019-03-06 15:30:08 +0300

    add role to backup users home into datawaha

---
+++ roles/user-data-backup/files/noor-backup-everyone.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+##############################################################################
+## NOOR backup for every user under /home/
+## will exclude listed files and files bigger than 50mb
+## This is meant to be run as root
+##############################################################################
+
+MAXSIZE="500m"
+# get the local machine name
+hostname=$(hostname -s)
+
+# Exclude pattern for rsync
+excludelist=/tmp/.noor-exclude.list
+cat > $excludelist << EOF
+*.o
+*.cu_o
+#.svn/
+#.git/
+#*.so
+#*.a
+#*.la
+*.pyo
+#*.pyc
+# Asuming user is doing backup on his/her own.
+/noor-backup/
+EOF
+
+DSTHOST="dm.kaust.edu.sa"
+remotepath="/datawaha/ecrc/gonzalea/ALLECRC/$hostname"
+
+# lower priority
+renice -n 5 -p $$ || true
+
+for userdir in `find /home/ -maxdepth 1 -type d `; do
+	# get username and check uid
+	username=$(basename $userdir)
+	id=$(id -u $username 2>/dev/null )
+	[ -n "$id" ] && [ $id -gt 100000 ] || continue;
+
+	#TODO: backup only recent files
+	#find /path/to/dir -mtime -366 > /tmp/rsyncfiles # files younger than 1 year
+	#rsync -Ravh --files-from=/tmp/rsyncfiles / root@www.someserver.com:/root/backup
+
+	last=`lastlog -u $username -t 180` # did she/he connect in last 180 days
+	if [ "a$last" == "a" ]; then
+		# no 
+		echo "User has not connected in last 180 days"
+		continue
+	fi
+
+
+	# Check if user can connect passworless.
+	# This indicates that a personal back must be working.No need for this
+	if sudo -u $username ssh -o "NumberOfPasswordPrompts 0" $DSTHOST ls /rcsdata/ecrc/$username/$hostname 1>&2 2>/dev/null
+	then
+		echo "User $username must be doing backup."
+		continue
+	fi
+
+	# ionice to avoid stressing the system (best-effort)
+	# use delete-delay because is more efficient than delete-after
+	ionice -c 2 -n 6 -t rsync -e 'ssh -i /home/gonzalea/.ssh/id_rsa -o "NumberOfPasswordPrompts 0"' -az -vv --exclude-from=$excludelist --delete-delay --delete-excluded --max-size=$MAXSIZE $userdir/ gonzalea@$DSTHOST:$remotepath/$username || continue;
+
+	sleep 5
+done
---
+++ roles/user-data-backup/files/noor-backup-folder.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+##################################################################################
+## Requirements for the NOOR backup
+## 1) Request a Noor account and ask to be part of rc-ecrc group (IT HelpDesk)
+## 2) Set up a passwordless ssh connection to dm.kaust.edu.sa (using ssh keys)
+## 3) Check your passwordless connection and see if the 
+##    directory /rcsdata/ecrc/<your-username> exists in dm.kaust.edu.sa
+##################################################################################
+
+LOCALDIR=$HOME/noor-backup
+LOCALDIR=$HOME
+REMOTEHOST=dm.kaust.edu.sa
+
+# get the local machine name
+hostname=$(hostname -s)
+
+# if directory doesn't exist, exit
+[ -d $LOCALDIR ] || exit 101;
+
+# if userid is less than 100000, exit
+id=$(id -u $USER)
+[ $id -gt 100000 ] || exit 102;
+
+attempts=0
+MAXATTEMPTS=1
+
+REMOTEPATH="/datawaha/ecrc/$USER/$hostname"
+
+while [ $attempts -lt $MAXATTEMPTS ] ; do 
+	attempts=$((attempts + 1));
+	# if max attempts reached, try to use 'real' path in noor
+#	[ $attempts -eq $((MAXATTEMPTS)) ] && REMOTEPATH="/grs_data/labs/ecrc/$USER/$hostname"
+
+	# copy data, deleting files that no longer exists locally.
+	# Files are stored in the path /rcsdata/ecrc/$USER/$hostname at NOOR
+	# Files are kept for 60 days in IT's own backup system, just in case.
+	ionice -c 2 -n 6 -t rsync -e 'ssh -o "NumberOfPasswordPrompts 0"' -a --delete $LOCALDIR/ $REMOTEHOST:$REMOTEPATH && break;
+done
+
+exit $((attempts-1))
+
